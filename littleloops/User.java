5a239d948d7ae565e10e4c70bb43f0340d5f6307
PrattMercy <prattmercy3@gmail.com>
2026-04-09 05:24:58 +0100

    Task

---
+++ littleloops/User.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class User{
+    public static void main(String [] args){
+    Scanner inputCollector = new Scanner(System.in);
+        System.out.print("Enter integer ");
+       int N = inputCollector.nextInt();
+            for (int i=1; i<= N; i++){
+               System.out.println(i);
+            }
+        }
+    }
