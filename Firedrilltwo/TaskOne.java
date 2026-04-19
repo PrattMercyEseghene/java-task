7524619eaae5aea5cc289ede9d031fc0b4250142
PrattMercy <prattmercy3@gmail.com>
2026-04-19 23:41:44 +0100

    sunday

--- Firedrilltwo/TaskOne.java
+++ Firedrilltwo/TaskOne.java
@@ -4,31 +4,5 @@ import java.util.Scanner;
  public static void main( String[] args)
   scanner inputCollector = new scanner (System.in);
   System.out.println("Enter  First Score");
-   int FirstScore = inputCollector.nextInt();
-
-  System.out.println("Enter Second Score");
- int SecondScore = inputCollector.nextInt();
-
- System.out.println("Enter Third Score");
-int ThirdScore = inputCollector.nextInt();
-
- System.out.println("Enter Fourth Score");
- int FourthScore = inputCollector.nextInt();
-System.out.println("Enter Fifth Score");
-int FifthScore = inputCollector.nextInt();
-System.out.println("Enter  Sixth Score");
-    int Sixth Score = inputCollector.nextInt();
-
-  System.out.println("Enter Seventh Score");
- int Seventh Score = inputCollector.nextInt();
-
- System.out.println("Enter Eight Score");
-int EightScore = inputCollector.nextInt();
-
- System.out.println("Enter Nineth Score");
- int NinethScore = inputCollector.nextInt();
-System.out.println("Enter Tenth Score");
-int TenthScore = inputCollector.nextInt();
-Total Score = FirstScore + SecondScore + ThirdScore + FourthScore + FifthScore + SixthScore + SeventhScore + EightScore + NinethScore + TenthScore
-System.out.println("")
+   
 
