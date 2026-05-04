24de54f401dc2acf31c33d12344df29e3ba14bbb
PrattMercy <prattmercy3@gmail.com>
2026-05-04 18:44:19 +0100

    FiredrillThree

---
+++ FiredrillThree/TaskOne.java
@@ -0,0 +1,34 @@
+import java. util.Scanner;
+public class TaskOne{
+public static void main(String[]args){
+
+Scanner inputCollector = new Scanner(System.in);
+System.out.println("Enter ten scores");
+int scores = inputCollector.nextInt();
+
+for(int count = 1; count <=scores; count++){
+
+scores = inputCollector.nextInt();
+}
+
+System.out.println(scores);
+} 
+}
+
+
+
+
+
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
